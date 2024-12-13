44f032ae66bfb1da18e4d84cf33ad70288e86c22
M.Badiane <tonsbadiane13@gmail.com>
2024-12-13 22:55:12 +0000

    https://github.com/Tonsgeezer/sakurawear.git

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Site Sakura Wear chargé avec succès !");
+    // Vous pouvez ajouter des interactions ici si nécessaire.
+});
+
